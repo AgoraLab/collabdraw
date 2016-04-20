e103d1a88ff41e98008d82cb44de135d1c7424df
George Sun <sunwinner3@gmail.com>
2016-04-20 14:46:35 +0800

    Accepting URL parameter of room name with demo pages..

--- client/js/entry.js
+++ client/js/entry.js
@@ -7,9 +7,9 @@
             var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
                 results = regex.exec(url);
 
-                if (!results) return null;
-                if (!results[2]) return '';
-                return decodeURIComponent(results[2].replace(/\+/g, " "));
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
         }
 
         var width  = getParameterByName('width');
