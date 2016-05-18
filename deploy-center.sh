c3d4d2e2b4a087924572bf83afadf7a2b35c3cfc
frank <linshibo3456@gmail.com>
2016-05-18 17:59:38 +0800

    change run_edge deploy_edge

--- deploy-center.sh
+++ deploy-center.sh
@@ -3,3 +3,5 @@ cd `dirname $0`
 rsync -avzr ./center_server/* --exclude=__pycache__ --rsh "ssh -p 22 -i $HOME/.ssh/devops.pem" devops@119.9.92.228:~/collabdraw/center_server;
 ssh -o "StrictHostKeyChecking no" -p 20220 -i ~/.ssh/devops.pem devops@119.9.92.228 "~/collabdraw/center_server/run_center"
 
+rsync -avzr ./center_server/* --exclude=__pycache__ --exclude=*.pyc --rsh "ssh -p 22 -i $HOME/.ssh/devops.pem" devops@139.224.34.246:~/collabdraw/center_server;
+ssh -o "StrictHostKeyChecking no" -p 20220 -i ~/.ssh/devops.pem devops@139.224.34.246 "~/collabdraw/center_server/run_center"
